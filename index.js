cf88c873f5ebfaf485626a79a2c068aeb96c1031
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-22 16:22:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ni8nft9.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // MongoDB collections
+    const taskCollections = client.db("task-management").collection("tasks");
+
+    // store task data
+    app.post('/tasks', async(req,res)=>{
+        const task = req.body;
+        console.log(task);
+        // const result = await taskCollections.insertOne(task);
+        // res.send(result);
+    })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
